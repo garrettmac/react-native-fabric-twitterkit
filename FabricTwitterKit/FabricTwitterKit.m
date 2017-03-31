a59a37171a0b085df0c89d11fe8fb208c8abf2fc
Vyga <garrettmacmac@gmail.com>
2017-03-30 18:42:45 -0600

    Update FabricTwitterKit.m
    added an API method

--- FabricTwitterKit/FabricTwitterKit.m
+++ FabricTwitterKit/FabricTwitterKit.m
@@ -1,6 +1,6 @@
 //
 //  FabricTwitterKit.m
-//  FabricTwitterKit
+//  FabricTwitterKit Api
 //
 //  Created by Trevor Porter on 8/1/16.
 //  Copyright © 2016 Trevor Porter. All rights reserved.
@@ -10,9 +10,9 @@
 //  Licensed under the MIT License. See the LICENSE file in the project root for license information.
 
 #import "FabricTwitterKit.h"
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventDispatcher.h>
-#import <React/RCTBridge.h>
+#import "React/RCTBridgeModule.h"
+#import "React/RCTEventDispatcher.h"
+#import "React/RCTBridge.h"
 //#import <Crashlytics/Crashlytics.h>
 #import <TwitterKit/TwitterKit.h>
 
@@ -20,6 +20,73 @@
 @synthesize bridge = _bridge;
 
 RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(Api:(NSDictionary *)options :(RCTResponseSenderBlock)callback)
+{
+    TWTRAPIClient *client = [[TWTRAPIClient alloc] init];
+    TWTRSessionStore *store = [[Twitter sharedInstance] sessionStore];
+    NSString *q = options[@"q"];
+    NSString *endpoint = options[@"endpoint"];
+    NSString *base = @"https://api.twitter.com/1.1/";
+    TWTRSession *lastSession = store.session;
+    NSMutableDictionary* params = [[NSMutableDictionary alloc] init];
+    for (NSString *aSubKey in [options allKeys]) {
+        NSString *aSubValue = [options objectForKey:aSubKey];
+
+        //NSLog(@"SubKey : %@, SubValue = %@", aSubKey, aSubValue);
+        [params setObject:aSubValue forKey:aSubKey];
+        //        if ([[options objectForKey:aSubKey] isEqualToNumber: [NSNumber class]]) {
+        //            NSNumber *aSubValue= [options objectForKey:aSubKey];
+        //            [params setObject:aSubValue forKey:aSubKey];
+        //        } else if ([[options objectForKey:aSubKey] isEqualToValue: [NSValue class]]) {
+        //            NSValue *aSubValue= [options objectForKey:aSubKey];
+        //            [params setObject:aSubValue forKey:aSubKey];
+        //        } else if ([[options objectForKey:aSubKey] isKindOfClass: [NSString class]]) {
+        //            NSString *aSubValue= [options objectForKey:aSubKey];
+        //            [params setObject:aSubValue forKey:aSubKey];
+        //        }
+    }
+    NSLog(@"%@",params);
+    if(lastSession) {
+        NSString *showEndpoint = [NSString stringWithFormat:@"%@%@",base,endpoint];
+        [params removeObjectForKey:@"endpoint"];
+
+        NSError *clientError;
+        NSURLRequest *request = [client
+                                 URLRequestWithMethod:@"GET"
+                                 URL:showEndpoint
+                                 parameters:params
+                                 error:&clientError];
+
+        if (request) {
+            [client
+             sendTwitterRequest:request
+             completion:^(NSURLResponse *response,
+                          NSData *data,
+                          NSError *connectionError) {
+                 if (data) {
+                     NSError *jsonError;
+                     NSDictionary *json = [NSJSONSerialization
+                                           JSONObjectWithData:data
+                                           options:0
+                                           error:&jsonError];
+                     //                     NSLog(@"%@",[json description]);
+                     callback(@[json,[NSNull null]]);
+                 }
+                 else {
+                     NSLog(@"Error code: %ld | Error description: %@", (long)[connectionError code], [connectionError localizedDescription]);
+                     callback(@[[NSDictionary new],[connectionError localizedDescription]]);
+                 }
+             }];
+        }
+        else {
+            NSLog(@"Error: %@", clientError);
+        }
+
+    }
+
+}
+
 
 RCT_EXPORT_METHOD(login:(RCTResponseSenderBlock)callback)
 {
@@ -55,7 +122,7 @@ RCT_EXPORT_METHOD(fetchProfile:(RCTResponseSenderBlock)callback)
                                  parameters:params
                                  error:&clientError];
 
-          if (request) {
+        if (request) {
             [client
              sendTwitterRequest:request
              completion:^(NSURLResponse *response,
@@ -82,9 +149,7 @@ RCT_EXPORT_METHOD(fetchProfile:(RCTResponseSenderBlock)callback)
         }
 
     }
-    else {
-      callback(@[@"Session must not be null."]);
-    }
+
 
 }
 
@@ -101,10 +166,10 @@ RCT_EXPORT_METHOD(fetchTweet:(NSDictionary *)options :(RCTResponseSenderBlock)ca
     if(lastSession) {
         NSString *showEndpoint = @"https://api.twitter.com/1.1/statuses/show.json";
         NSDictionary *params = @{
-                                    @"id": id,
-                                    @"trim_user": trim_user,
-                                    @"include_my_retweet": include_my_retweet
-                                };
+                                 @"id": id,
+                                 @"trim_user": trim_user,
+                                 @"include_my_retweet": include_my_retweet
+                                 };
 
         NSError *clientError;
         NSURLRequest *request = [client
@@ -140,20 +205,24 @@ RCT_EXPORT_METHOD(fetchTweet:(NSDictionary *)options :(RCTResponseSenderBlock)ca
         }
 
     }
-    else {
-      callback(@[@"Session must not be null."]);
-    }
 
 }
 
 RCT_EXPORT_METHOD(composeTweet:(NSDictionary *)options :(RCTResponseSenderBlock)callback) {
 
-    NSString *body = options[@"body"];
+
+    NSURL *link =options[@"url"];
 
     TWTRComposer *composer = [[TWTRComposer alloc] init];
 
-    if (body) {
-        [composer setText:body];
+    if (options[@"text"]) {
+        [composer setText:options[@"text"]];
+    }
+    if (options[@"image"]) {
+        [composer setImage:[UIImage imageNamed:options[@"image"]]];
+    }
+    if (link) {
+        [composer setURL:link];
     }
 
     UIViewController *rootView = [UIApplication sharedApplication].keyWindow.rootViewController;
